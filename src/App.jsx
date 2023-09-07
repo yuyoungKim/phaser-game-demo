1a1f6daf77658ab1dd868eb7e963c4aa3c36507c
Yuyoung Kim <104450539+yuyoungKim@users.noreply.github.com>
2023-09-07 16:21:06 -0400

    Coverting to React Bootstrap and fixing error
    Fixing errors of background image and button error

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
-import './App.css';
-import video from './logo_animation.mp4'
 import React from 'react';
-
+import Routes from './Routes';
 
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="App-logo">
-        <video src={video} width="750" height="500" autoPlay={true} muted>
-          </video>
-        </div>
-
-        <h1 className="App-title">
-          Sort Game
-        </h1>
-        <h3 className="App-summary">
-          Summary
-        </h3> 
-      </header>
-      
-    </div>
+      <div className="App">
+        <Routes></Routes>
+      </div>
   );
 }
-
 
 export default App;
