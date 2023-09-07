42051b45919bb600288167006dd22a62502ea8e6
Yuyoung Kim <104450539+yuyoungKim@users.noreply.github.com>
2023-09-07 16:18:58 -0400

    Change the image directory
    Source to inside of src

---
+++ src/Home.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import React from 'react';
+import {Container, Button  } from 'react-bootstrap';
+import {Link} from "wouter";
+
+
+function Home() {
+  return (
+    <Container fluid className="Home-background">
+      <video className="w-100" autoPlay muted>
+        <source
+          src="logo_animation.mp4"
+          type="video/mp4"
+        />
+      </video>
+      <h1 className="Home-title">
+        Sort Game
+      </h1>
+      <Link href="/Game" >
+        <Button size="lg" className="Start-button" >Start game</Button>
+      </Link>
+    </Container>
+  );
+}
+
+export default Home;
